38f9e4c2a5dd98cc234252d4f21a0f2bf742a75e
Simranjit16 <51396306+Simranjit16@users.noreply.github.com>
2019-06-13 16:08:23 +0000

    Add files via upload

---
+++ date.py
@@ -0,0 +1,8 @@
+ #!/bin/user/python3
+ import datetime
+ name=input("your name :")
+ age=int(input("your age :"))
+ a = datetime.datetime.now()
+
+ print("you will turn 95 by the year:".format(name(95-age)+a.year)
+
